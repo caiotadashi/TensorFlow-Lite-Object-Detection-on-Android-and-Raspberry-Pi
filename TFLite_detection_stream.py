89629c2a09a093934983c7f74afc75e134580792
Caio Tadashi <ctdsh@icloud.com>
2021-07-03 16:54:32 -0300

    changed opencv2

--- TFLite_detection_stream.py
+++ TFLite_detection_stream.py
@@ -30,7 +30,7 @@ class VideoStream:
     def __init__(self,resolution=(640,480),framerate=30):
         # Initialize the PiCamera and the camera image stream
         self.stream = cv2.VideoCapture(STREAM_URL)
-        ret = self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
+        ret = self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'X264'))
         ret = self.stream.set(3,resolution[0])
         ret = self.stream.set(4,resolution[1])
             
